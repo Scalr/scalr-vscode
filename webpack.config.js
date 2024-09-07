b11d805a77603783bbe0d00014dfba1bc55a0c57
penja <a.penchuk@scalr.com>
2024-09-07 21:19:23 +0300

    Make webcompatible extension

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,76 @@
-//@ts-check
-
 'use strict';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const webpack = require('webpack');
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
+/** @type WebpackConfig */
+const webConfig = {
+    target: 'webworker', // extensions run in a webworker context
+    mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+
+    entry: {
+        'extension-web': './src/extension.ts',
+    },
+    output: {
+        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/'
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: 'commonjs2',
+    },
+    externals: {
+        vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        // modules added here also need to be added in the .vscodeignore file
+    },
+    resolve: {
+        mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
+        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: ['.ts', '.js'],
+        fallback: {
+            // Webpack 5 no longer polyfills Node.js core modules automatically.
+            // see https://webpack.js.org/configuration/resolve/#resolvefallback
+            // for the list of Node.js core module polyfills.
+            assert: require.resolve('assert'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser', // provide a shim for the global `process` variable
+        }),
+    ],
+    devtool: 'nosources-source-map',
+    performance: {
+        hints: false,
+    },
+};
 
 /** @type WebpackConfig */
-const extensionConfig = {
+const nodeConfig = {
     target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
     mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
-    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    entry: {
+        'extension-node': './src/extension.ts',
+    },
     output: {
         // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, 'dist'),
-        filename: 'extension.js',
+        filename: '[name].js',
         libraryTarget: 'commonjs2',
     },
     externals: {
@@ -45,5 +98,8 @@ const extensionConfig = {
     infrastructureLogging: {
         level: 'log', // enables logging required for problem matchers
     },
+    performance: {
+        hints: false,
+    },
 };
-module.exports = [extensionConfig];
+module.exports = [nodeConfig, webConfig];
