51747c143b81b60f458f35f6eb02405a5335fa26
penja <a.penchuk@scalr.com>
2024-09-28 17:48:32 +0300

    Fix the web extension

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
